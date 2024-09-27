001d5efd3a76bc5740c4fc60bba5337acd48ce12
guneeshvats <70188630+guneeshvats@users.noreply.github.com>
2024-09-27 15:43:43 +0530

    Update mp3_to_wav.py

--- scripts/mp3_to_wav.py
+++ scripts/mp3_to_wav.py
@@ -1,3 +1,25 @@
+############################################################################################################################
+############################################################################################################################
+'''
+Purpose : Convert all MP3 files in a directory to WAV format using FFmpeg.
+
+Steps : 
+
+1. Create Output Directory: Creates a new folder if the specified output folder does not exist.
+2. Iterate through Files: Loops through each file in the input directory and checks if it is an MP3 file.
+3. Convert MP3 to WAV: Uses FFmpeg to convert each MP3 file to WAV format and saves it in the specified output directory.
+
+Replace Paths : 
+
+input_folder = "path_to_input_folder_containing_mp3_files"
+output_folder = "path_to_output_folder_for_wav_files"
+
+Code written by: Guneesh Vats
+Date: 30th Aug, 2024
+'''
+############################################################################################################################
+############################################################################################################################
+
 import os
 import subprocess
 
@@ -17,5 +39,6 @@ def convert_mp3_to_wav_with_ffmpeg(input_folder, output_folder):
             except subprocess.CalledProcessError as e:
                 print(f"Error converting {audio_file}: {e}")
 
-# Example usage:
-convert_mp3_to_wav_with_ffmpeg('/Users/guneeshvats/Desktop/Adalat AI assignment/court_audio_files', 'court_audio_files_wav')
+input_folder = "/Users/guneeshvats/Desktop/Adalat AI assignment/court_audio_files"
+output_folder = "court_audio_files_wav"
+convert_mp3_to_wav_with_ffmpeg(input_folder, output_folder)
